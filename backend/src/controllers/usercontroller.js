aea7ce0665829f81a558a23581805431c7a09412
Tobias Steffensen <Computer_X@outlook.dk>
2021-10-14 08:28:36 +0200

    token creation

--- backend/src/controllers/usercontroller.js
+++ backend/src/controllers/usercontroller.js
@@ -4,17 +4,26 @@ const jwt = require('jsonwebtoken');
 exports.auth = async (req, res) => {
     let username = req.body.Username;
     let password = req.body.Password;
-    if (username && password) {
-        let results = await db.pool.query('SELECT * FROM Users WHERE username = ?', [username]);
-        if (results.length > 0) {
-            console.log(results[0]);
 
-            res.status(200).json('Correct Username and Password!');
+    try {
+        if (username && password) {
+            let results = await db.pool.query('SELECT * FROM Users WHERE username = ?', [username]);
+            let user = results[0];
+
+            if (results.length == 0) {
+                return res.status(500).json('Username does not exist!');
+            }
+
+            let token = jwt.sign({ username: user.Username, email: user.Email }, process.env.TOKEN_KEY, { expiresIn: '2h' });
+            user.Token = token;
+            await db.pool.query('UPDATE Users SET Token=? WHERE ID=?', [token, user.ID]);
+            return res.status(200).json(user);
         } else {
-            res.status(500).json('Incorrect Username and/or Password!');
+            return res.status(500).json('Please enter Username and Password!');
         }
-    } else {
-        res.status(500).json('Please enter Username and Password!');
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Internal server error');
     }
 };
 
@@ -24,17 +33,21 @@ exports.register = async (req, res) => {
     let fullname = req.body.FullName;
     let email = req.body.Email;
 
-    if (username && password && fullname && email) {
-        let results = await db.pool.query('SELECT * FROM Users WHERE username = ?', [username]);
-        if (results.length > 0) {
-            return res.status(500).json('Username already exists!');
+    try {
+        if (username && password && fullname && email) {
+            let results = await db.pool.query('SELECT * FROM Users WHERE username = ?', [username]);
+            if (results.length > 0) {
+                return res.status(500).json('Username already exists!');
+            }
+
+            await db.pool.query('INSERT INTO Users (Username, Password, FullName, Email) VALUES (?, ?, ?, ?)', [username, password, fullname, email]);
+
+            return res.status(201).json('User registered successfully!');
+        } else {
+            return res.status(500).json('Please enter Username, Password, FullName and Email');
         }
-
-        let token = jwt.sign({ username, email }, process.env.TOKEN_KEY, { expiresIn: '2h' });
-        await db.pool.query('INSERT INTO Users (Username, Password, FullName, Email, Token) VALUES (?, ?, ?, ?, ?)', [username, password, fullname, email, token]);
-
-        return res.status(201).json('User registered successfully!');
-    } else {
-        return res.status(500).json('Please enter Username, Password, FullName and Email');
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Internal server error');
     }
 };
