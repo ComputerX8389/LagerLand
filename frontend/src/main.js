056c1680399f53eda6b25e978a1d6a16fdec6d98
Tobias Steffensen <Computer_X@outlook.dk>
2021-10-15 12:30:20 +0200

    Login button

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import PrimeVue from 'primevue/config';
+import './registerServiceWorker';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue);
+// Primevue components
+import 'primeflex/primeflex.css';
+import 'primevue/resources/themes/saga-blue/theme.css'; //theme
+import 'primevue/resources/primevue.min.css'; //core css
+import 'primeicons/primeicons.css'; //icons
+
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+app.component('Button', Button);
+app.component('InputText', InputText);
+
+app.mount('#app');
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,9 +1,32 @@
 <template>
-    <h1>login</h1>
+    <InputText type="username" v-model="username" />
+    <InputText type="password" v-model="password" />
+    <Button label="Login" @click="onClick" />
 </template>
 
 <script>
-export default {};
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            username: '',
+            password: '',
+        };
+    },
+    methods: {
+        async onClick() {
+            console.log('username', this.username);
+            console.log('password', this.password);
+
+            let response = await axios.post('http://localhost:3000/Auth', {
+                Username: this.username,
+                Password: this.password,
+            });
+            let token = response.data.Token;
+            console.log('token', token);
+        },
+    },
+};
 </script>
 
 <style></style>
